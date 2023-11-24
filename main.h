dd08ceddcd31f123b5d47fd7ec055f2442ee5d8f
MrSamy91 <samy.ajouid@gmail.com>
2023-11-23 18:41:45 -0800

    angement

--- main.h
+++ main.h
@@ -1,14 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
 
 
 
 
-#include <stdarh.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 #endif
--- second_task.c
+++ second_task.c
@@ -1,46 +1,44 @@
 #include "main.h"
-/*
- * Fonction _printf qui imite partiellement la fonction printf du langage C
- * Elle prend un format et une liste variable d'arguments
- */
-int main(void)
-{
-return 0;
-}
+#include <stdarg.h>
 
-int _printf(const char *format, ...) 
-{
+int _printf(const char *format, ...) {
 	int count = 0; /* Variable pour suivre le nombre de caractères imprimés */
 	va_list args; /* Déclaration de la liste d'arguments variable */
-	va_start (args, format); /* Initialisation de la liste d'arguments variable avec le format */
+	va_start(args, format); /* Initialisation de la liste d'arguments variable avec le format */
 
-    while (*format != '\0')
-	{
+	while (*format != '\0') {
 		/* Gestion des spécificateurs de conversion */
-		if (*format == 'd' || *format == 'i') 
-		{
-                	/* Gérer %d et %i : entier signé */
-                	int num = va_arg(args, int);
-			printf("%d", num);
+		if (*format == 'd' || *format == 'i') {
+			/* Gérer %d et %i : entier signé */
+			int num = va_arg(args, int);
+			/* Utilisation de putchar pour imprimer un seul caractère à la fois */
+			putchar(num + '0'); /* Convertit l'entier en caractère et l'imprime */
 			count++;
-		}
-		else if (*format == 'c')
-		{
+		} else if (*format == 'c') {
 			/* Gérer %c : caractère */
 			char ch = va_arg(args, int); /* Va_arg pour un caractère doit retourner int */
+			/* Utilisation de putchar pour imprimer un seul caractère à la fois */
 			putchar(ch);
 			count++;
+		} else {
+			/* Caractère ordinaire, l'imprimer */
+			/* Utilisation de putchar pour imprimer un seul caractère à la fois */
+			putchar(*format);
+			count++;
 		}
-		else 
-		{
-            	/* Caractère ordinaire, l'imprimer */
-            	putchar(*format);
-            	count++;
-        	}
+
+		format++; /* Passer au caractère suivant dans la chaîne de format */
 	}
 
-    format++;  /* Passer au caractère suivant dans la chaîne de format */
-    va_end(args);  /* Nettoyer la liste d'arguments variable */
+	va_end(args); /* Nettoyer la liste d'arguments variable */
 
-    return count;
+	return count;
 }
+
+int main(void) {
+	/* Utilisation de _printf avec putchar pour l'impression */
+	_printf("Hello, %c! This is a %d example.\n", 'W', 42);
+
+	return 0;
+}
+
